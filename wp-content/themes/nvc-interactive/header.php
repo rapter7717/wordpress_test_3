d93bdd31748e8a6312bf976f07cd6da5db1da8dc
rapter7717 <35534590+rapter7717@users.noreply.github.com>
2018-04-30 23:42:06 -0500

    Google Font

--- wp-content/themes/nvc-interactive/header.php
+++ wp-content/themes/nvc-interactive/header.php
@@ -16,6 +16,8 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="stylesheet"
+	    href="https://fonts.googleapis.com/css?family=Josefin+Sans">
 
 	<?php wp_head(); ?>
 </head>
